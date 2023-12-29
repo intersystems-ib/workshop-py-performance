da13b2094fd74991080290ac39bc9f16fabb4a56
isc-lperezra <lperezra@intersystems.com>
2023-12-29 09:04:34 +0100

    First commit

---
+++ mysql/sql/dump.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXISTS test;
+
+USE test;
+
+CREATE TABLE patient (
+    Id int(8) NOT NULL AUTO_INCREMENT,
+    Name varchar(225),
+    Lastname varchar(225),
+    Photo varchar(5000),
+    Phone varchar (14),
+    Address varchar (225),
+    PRIMARY KEY (Id)
+) ENGINE=INNODB;
